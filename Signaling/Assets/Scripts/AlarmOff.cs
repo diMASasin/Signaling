cda0df453de1df1ff10f6954c50418370fe9f509
diMASasin <35368058+diMASasin@users.noreply.github.com>
2020-12-08 22:34:02 +0500

    init commit

---
+++ Signaling/Assets/Scripts/AlarmOff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmOff : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _duration;
+
+    private float _runningTime;
+    private float _normalizedRunningTime;
+    private bool _needToOff = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player) && _audioSource.isPlaying)
+        {
+            _needToOff = true;
+        }
+    }
+
+    public void Update()
+    {
+        if(_audioSource.isPlaying && _needToOff)
+        {
+            _runningTime += Time.deltaTime;
+            _normalizedRunningTime = _runningTime / _duration;
+            _audioSource.volume = 1 - _normalizedRunningTime;
+
+            if (_audioSource.volume == 0)
+            {
+                _audioSource.Stop();
+                _runningTime = 0;
+                _normalizedRunningTime = 0;
+                _needToOff = false;
+            }
+        }
+    }
+}
---
+++ Signaling/Assets/Scripts/AlarmOn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmOn : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _duration;
+
+    private float _runningTime;
+    private float _normalizedRunningTime;
+    private bool _VolumeIsNotFull = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+                _VolumeIsNotFull = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_audioSource.isPlaying && _VolumeIsNotFull)
+        {
+            _runningTime += Time.deltaTime;
+            _normalizedRunningTime = _runningTime / _duration;
+            _audioSource.volume = _normalizedRunningTime;
+
+            if(_audioSource.volume == 1)
+            {
+                _VolumeIsNotFull = false;
+                _runningTime = 0;
+                _normalizedRunningTime = 0;
+            }
+        }
+    }
+}
